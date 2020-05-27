3a4f40489eae8babb14c17ddd28cc173dd897530
macke945 <55479431+macke945@users.noreply.github.com>
2020-05-27 13:56:30 +0200

    testing

---
+++ QFun/Controllers/ChallengesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QFun.Models;
+
+namespace QFun.Controllers
+{
+    public class ChallengesController : Controller
+    {
+        private readonly ILogger<ChallengesController> _logger;
+
+        public ChallengesController(ILogger<ChallengesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+      
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
