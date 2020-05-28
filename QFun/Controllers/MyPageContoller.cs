07942533e69aa8113d551a433d8cc477f10b996b
abrei852 <abbe1334@gmail.com>
2020-05-28 09:50:00 +0200

    Added UserData

--- QFun/Controllers/MyPageContoller.cs
+++ QFun/Controllers/MyPageContoller.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -23,6 +24,36 @@ namespace QFun.Controllers
             return View();
         }
 
+        public IActionResult _Chart()
+        {
+            List<UserData> newlist = new List<UserData>();
+
+            var tempModel = (from a in appc.Challenge
+                             join b in appc.Contribution on a.Id equals
+                             b.ChallengeId
+                             join c in appc.Vote on b.Id equals c.ContributionId
+                             select new { a.ChName, b.CoName, c.Votes, b.IdentityUserId });
+
+            var identity = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            foreach (var item in tempModel)
+            {
+
+                var userdata = new UserData();
+
+                if (item.IdentityUserId == identity)
+                {
+                    userdata.ChName = item.ChName;
+                    userdata.CoName = item.CoName;
+                    userdata.Votes = item.Votes;
+                    newlist.Add(userdata);
+
+                }
+
+            }
+
+            return View(newlist);
+        }
       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ QFun/Models/UserData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QFun.Models
+{
+    public class UserData
+    {
+        public string ChName { get; set; }
+
+        public string CoName { get; set; }
+        public int Votes { get; set; }
+        
+    }
+}
